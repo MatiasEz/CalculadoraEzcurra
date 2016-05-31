bbbae0c223f1faae4f96db49df833dcb0932a21c
Ezcurra <matialeezcurra@gmail.com>
2016-05-30 21:44:37 -0300

    Primer ejemplo calculadora

---
+++ EzcurraCalculadora/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EzcurraCalculadora
+//
+//  Created by Federicuelo on 30/5/16.
+//  Copyright © 2016 Ezcurra Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EzcurraCalculadora/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EzcurraCalculadora
+//
+//  Created by Federicuelo on 30/5/16.
+//  Copyright © 2016 Ezcurra Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
